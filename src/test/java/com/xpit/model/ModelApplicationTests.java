42eced9c8b8787dac9551e03f092d91a1d939d43
Renato Sanches <cwebmundi@gmail.com>
2019-12-18 09:19:53 -0200

    Projeto Criado

---
+++ src/test/java/com/xpit/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpit.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
