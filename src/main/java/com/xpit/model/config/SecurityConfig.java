16c5d00ecc1772197a31e45a8cfe9261a82a2696
Renato Sanches <cwebmundi@gmail.com>
2020-01-24 14:03:11 -0200

    add novos produtos e liberacao de cors para put e delete - ajuste

--- src/main/java/com/xpit/model/config/SecurityConfig.java
+++ src/main/java/com/xpit/model/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
 		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
 	
